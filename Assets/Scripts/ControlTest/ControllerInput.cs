61d665138537e53df64e45b2e603970b48a7380b
MateuszBlachacz <55129562+MateuszBlachacz@users.noreply.github.com>
2021-02-25 00:48:24 +0100

    Add own Controll Manager

---
+++ Assets/Scripts/ControlTest/ControllerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ControllerInput : MonoBehaviour
+{
+    public static ControllerInput self;
+
+    public KeyCode jump { get; set; }
+    public KeyCode forward { get; set; }
+    public KeyCode backward { get; set;}
+    public KeyCode left { get; set; }
+    public KeyCode right { get; set; }
+
+
+    private void Awake()
+    {
+        if(null == self)
+        {
+            DontDestroyOnLoad(gameObject);
+            self = this;
+        } else if(this != self ){
+            Destroy(gameObject);
+        }
+
+        jump = setKey("jumpedKey", "Space");
+        forward = setKey("forwardKey", "W");
+        backward = setKey("backwordKey", "S");
+        left = setKey("leftKey", "A");
+        right = setKey("rightKey", "D");
+
+    }
+
+    private KeyCode setKey(string actionName, string keyName)
+    {
+        return (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(actionName, keyName));
+    }
+}
---
+++ Assets/Scripts/ControlTest/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [ SerializeField ]
+    private float speed = 800f;
+    [ SerializeField ]
+    private float jumpForce = 4500f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(ControllerInput.self.forward))
+        {
+            rb.AddForce(Vector3.forward * Time.deltaTime * speed);
+        } else if (Input.GetKey(ControllerInput.self.backward))
+        {
+            rb.AddForce(Vector3.back * Time.deltaTime * speed);
+        } else if (Input.GetKey(ControllerInput.self.left))
+        {
+            rb.AddForce(Vector3.left * Time.deltaTime * speed);
+        } else if (Input.GetKey(ControllerInput.self.right))
+        {
+            rb.AddForce(Vector3.right * Time.deltaTime * speed);
+        } else if (Input.GetKeyDown(ControllerInput.self.jump))
+        {
+            rb.AddForce(Vector3.up * Time.deltaTime * jumpForce);
+        }
+
+
+        
+    }
+}
